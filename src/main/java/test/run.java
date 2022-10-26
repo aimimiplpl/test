d66dab8cdc87989f521c755c89abe9aaba5d000c
“aimimiplpl” <379015919@qq.com>
2022-10-26 20:06:18 +0800

    Initial commit

---
+++ src/main/java/test/run.java
@@ -0,0 +1,14 @@
+package test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class run {
+    public static void main(String[] args) {
+    ArrayList<HashMap<String,String>> a=new ArrayList<>();
+    HashMap<String,String> b =new HashMap<>();
+    b.put("key","123");
+    a.add(b);
+        System.out.println(a);
+    }
+}
